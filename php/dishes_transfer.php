98b48843c2f072165e449826f7b0cd4f4ce8cdf4
Micahdo <124348225+Micahdo@users.noreply.github.com>
2023-05-15 16:58:49 +0200

    Modification des termes

--- php/dishes_transfer.php
+++ php/dishes_transfer.php
@@ -3,12 +3,13 @@
     $dishDesc = $_POST['dishDesc'];
     $dishPrice = $_POST['dishPrice'];
     $dishCategory = $_POST['dishCategory'];
+
     include_once('../php/bdd.php');
-    $selectStatement = $db->prepare("SELECT title FROM dishes WHERE title='$dishName'");
-    $selectStatement->execute();
-    $select = $selectStatement->fetch();
-    if ($select != null)
-    {
+    $statement = $db->prepare("SELECT title FROM dishes WHERE title=:dishTitle");
+    $statement->bindValue(':dishTitle', $dishName);
+    $statement->execute();
+    $select = $statement->fetch();
+    if ($select != '') {
         $delRow = $db->prepare("UPDATE dishes SET title=:title, description=:description, price=:price, category=:category WHERE title = :title");
         $delRow->bindParam(':title', $dishName);
         $delRow->bindParam(':description', $dishDesc);
@@ -16,8 +17,12 @@
         $delRow->bindParam(':category', $dishCategory);
         $delRow->execute();
     } else {
-        $insert = $db->prepare("INSERT INTO dishes (title, description, price, category) VALUES ('$dishName', '$dishDesc', '$dishPrice', '$dishCategory')");
-        $insert->execute();
+        $insertRow = $db->prepare("INSERT INTO dishes (title, description, price, category) VALUES (:dishName, :dishDesc, :dishPrice, :dishCategory)");
+        $insertRow->bindValue(':dishName', $dishName);
+        $insertRow->bindValue(':dishDesc', $dishDesc);
+        $insertRow->bindValue(':dishPrice', $dishPrice);
+        $insertRow->bindValue(':dishCategory', $dishCategory);
+        $insertRow->execute();
     };   
     header('location:../administration.php');
 ?>
