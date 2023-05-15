683a05e7c90ae83d156dc65921f37bd6f35a0466
Micahdo <124348225+Micahdo@users.noreply.github.com>
2023-05-15 17:01:13 +0200

    Add files via upload

--- js/validation.js
+++ js/validation.js
@@ -1,12 +1,5 @@
-let user_email = document.getElementById('user_email');
-let user_password = document.getElementById('user_password');
-let email = document.getElementById('email');
-let password = document.getElementById('password');
-let username = document.getElementById('username');
-let convives = document.getElementById('convives');
-let emailRegex = /^[0-9a-zA-Z_!#$%&'*+\/=?`{|}~^.-]+@[0-9a-zA-Z]+\.[0-9a-zA-Z]+$/;
-
 $(document).ready(function(){
+    let emailRegex = /^[0-9a-zA-Z_!#$%&'*+\/=?`{|}~^.-]+@[0-9a-zA-Z]+\.[0-9a-zA-Z]+$/;
     //Validation de la connexion au compte
     $('#btn-connect').on('click', function(e){
         if (user_email.value.trim()==''){
@@ -36,7 +29,7 @@ $(document).ready(function(){
             alert("Veuillez entrer un identifiant");
         };
     });
-    //Envoié des données à user_new_account.php
+    //Envoi des données à user_new_account.php
     $('#btn-creation').on('click', function(e){
         if (email.value.trim()!='' && password.value.trim()!='' && username.value.trim()!='' && emailRegex.test(email.value)!=false){
             email = $('#email').val();
