fbbfb51e6026f90ffa43eaa8406084e53db34e6b
Micahdo <124348225+Micahdo@users.noreply.github.com>
2023-05-18 14:25:17 +0200

    Modification de la navigation

--- navigation.php
+++ navigation.php
@@ -1,7 +1,8 @@
 <div class="navbar flexRow">
     <ul class="flexRow">
-        <li><a href="carte.php">Carte du restaurant</a></li>
-        <li><a href="reservation.php">Réservation</a></li>
+        <li><a href="carte.php">Carte</a></li>
+        <li><a href="menus.php">Menus</a></li>
         <li><a href="user.php">Connexion</a></li>
+        <li><a href="reservation.php">Réservation</a></li>
     </ul>
 </div>
