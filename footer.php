43982078ef93d4f2f979e65493777205b11dacdf
Micahdo <124348225+Micahdo@users.noreply.github.com>
2023-05-18 14:26:05 +0200

    Modification du footer

--- footer.php
+++ footer.php
@@ -2,22 +2,28 @@
     include('php/bdd.php');
 ?>
 
-<div class="flexRow footerDiv">
+<div class="flexArticle">
     <div class="footerHours">
-        <h4>HORAIRES D'OUVERTURE</h4>
-        <div class="flexRow around">
+        <h4>HORAIRES D'OUVERTURE</h4>        
+        <div class="flexHours">
             <?php
                 include_once('php/hours_print.php');
             ?>
         </div>
     </div>
-    <div class="footerAddress">
-        <h4>ADRESSE</h4>
-        <p>24 rue Machin-Chose</p>
-        <p>73000 CHAMBÉRY</p>
-    </div>
-    <div class="footerPhone">
-        <h4>TÉLÉPHONE</h4>
-        <p>04.50.50.50.50</p>
+    <div class="flexRow footerContact around">
+        <div>
+            <h4>ADRESSE</h4>
+            <p>24 rue Machin-Chose</p>
+            <p>73000 CHAMBÉRY</p>
+        </div>
+        <div>
+            <h4>TÉLÉPHONE</h4>
+            <p>04.50.50.50.50</p>
+        </div>
     </div>
 </div>
+
+<?php
+    $db=null;
+?>
