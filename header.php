5ed5fd3f41afb9d15533d28fe72e8383325d3d94
Micahdo <124348225+Micahdo@users.noreply.github.com>
2023-05-18 14:25:52 +0200

    Modification du header

--- header.php
+++ header.php
@@ -1,26 +1,35 @@
-<div class="headerDiv">
+<div>
     <h1><a href="index.php" class="titreSite txtWhite ">Quai Antique</a></h1>
 </div>
 
 <?php
 if (isset($_SESSION['email'])) {
-    if ($_SESSION['admin'] == 1000) {
+    if ($_SESSION['admin'] === 1000) {
         $user = $_SESSION['user'];
-        echo '<div class="userConnected">';
-        //echo '<p>Bienvenue '.$user.'</p>';
-        echo '<a href="administration.php" class="linkAdmin">Bienvenue '.$user.'</a>';
+        echo '<div class="userConnected flexRow">';
+        echo '<a href="administration.php">
+        <img src="images/user.png" class="icon">
+        </a>';
         echo '<form action="php/disconnect.php" method="post">
-        <button class="btnDisconnect">Déconnexion</button>
+        <button class="btn-icon">
+        <img src="images/logout.png" class="icon">
+        </button>
         </form>';
         echo '</div>';
     } else if ($_SESSION['admin'] === 0) {
         $user = $_SESSION['user'];
-        echo '<div class="userConnected">';
-        echo '<p>Bienvenue '.$user.'</p>';
+        echo '<div class="userConnected flexRow">';
+        echo '<a href="user_account.php">
+        <img src="images/user.png" class="icon">
+        </a>';
         echo '<form action="php/disconnect.php" method="post">
-        <button class="btnDisconnect">Déconnexion</button>
+        <button class="btn-icon">
+        <img src="images/logout.png" class="icon">
+        </button>
         </form>';
         echo '</div>';
+
+
     }
 };
 ?>
